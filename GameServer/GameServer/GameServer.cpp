d83cd39e955fae25b4d2b6411283a4656f743d08
SYJ1029 <abg0986@gmail.com>
2025-09-02 15:44:47 +0900

    멀티쓰레드의 기본

--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -2,8 +2,34 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread() {
+
+    cout << "Hello Thread :)" << endl;
+}
+
 int main()
 {
     HelloWorld();
+
+    std::thread t;
+
+    auto id1 = t.get_id();
+    cout << id1 << endl;
+
+
+    t = std::thread(HelloThread);
+    auto id2 = t.get_id();
+    cout << id2 << endl;
+
+    int64 cnt = t.hardware_concurrency();
+
+    cout << cnt << endl;
+
+    cout << "Hello Main :(" << endl;
+
+
+    t.join();
 }
 
